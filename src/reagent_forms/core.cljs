774e2f8cad13a77d95faa804533a3297fa7eca89
Florian Klampfer <f.klampfer@gmail.com>
2016-02-26 13:27:43 +0100

    Set input type to number for :numeric inputs

--- src/reagent_forms/core.cljs
+++ src/reagent_forms/core.cljs
@@ -120,7 +120,7 @@
   (let [input-value (atom nil)]
     (render-element attrs doc
       [type (merge
-             {:type :text
+             {:type :number
               :value (or @input-value (get id ""))
               :on-change #(reset! input-value (value-of %))
               :on-blur #(do
@@ -198,7 +198,7 @@
                :default-checked (= value (get name))
                :on-change #(save! name value)}
               attrs)]
-       body)))
+      body)))
 
 (defmethod init-field :typeahead
   [[type {:keys [id data-source input-class list-class item-class highlight-class input-placeholder result-fn choice-fn clear-on-focus?]
@@ -249,7 +249,7 @@
                                                        (reset! selected-index 0))
                                                 "default"))}]
 
-                     [:ul {:style {:display (if (or (empty? @selections) @typeahead-hidden?) :none :block) }
+                     [:ul {:style {:display (if (or (empty? @selections) @typeahead-hidden?) :none :block)}
                            :class list-class
                            :on-mouse-enter #(reset! mouse-on-list? true)
                            :on-mouse-leave #(reset! mouse-on-list? false)}
